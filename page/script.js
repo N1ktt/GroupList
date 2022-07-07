fe89e600c06dbf44a89e1aee8e56b76fe13991c8
ErykPet <edzikxd@gmail.com>
2022-07-07 21:49:11 +0200

    Update script.js

--- page/script.js
+++ page/script.js
@@ -39,8 +39,8 @@ if(localStorage.getItem("CELLSPERATOR") == null)
 
 
 //get data and generate table
-// data = JSON.parse(localStorage.getItem("facebookGroupListData"))
-data = JSON.parse(`[{"documentId":"0346FC7B6D852132FD3D0623804A6F43","frameId":0,"result":{"indexes":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],"names":["_TEST","Zabrze Wczoraj Dziś Jutro","Szukam montażysty/grafika","Grafiki Płatne/Darmowe.","EKIPA YT! MONTAŻ GRAFIKA PŁATNE/PROMOCJA","Boomerawka - sekcja beki z boomerów.","VId","Zabrze dla każdego","Federacja Młodych Socjaldemokratów","Szukam Montażysty/Grafika","Społecznościawka","Informatycy sie zesrali","MONTAŻYŚCI NA YOUTUBE!","Etniczni Widzowie Pulchnego Niedźwiedzia","Ryfkowa Grupa","Wataha Esfara","(Grupa Nieaktualna)","akademia ninja verffiego 乡","YOU CANT JUST PUT THE FIBONACCI SPIRAL ON TOP OF WHATEVER THE FUCK YOU WANT","United Witkoland Empire"]}}]`)
+data = JSON.parse(localStorage.getItem("facebookGroupListData"))
+//data = JSON.parse(`[{"documentId":"0346FC7B6D852132FD3D0623804A6F43","frameId":0,"result":{"indexes":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],"names":["_TEST","Zabrze Wczoraj Dziś Jutro","Szukam montażysty/grafika","Grafiki Płatne/Darmowe.","EKIPA YT! MONTAŻ GRAFIKA PŁATNE/PROMOCJA","Boomerawka - sekcja beki z boomerów.","VId","Zabrze dla każdego","Federacja Młodych Socjaldemokratów","Szukam Montażysty/Grafika","Społecznościawka","Informatycy sie zesrali","MONTAŻYŚCI NA YOUTUBE!","Etniczni Widzowie Pulchnego Niedźwiedzia","Ryfkowa Grupa","Wataha Esfara","(Grupa Nieaktualna)","akademia ninja verffiego 乡","YOU CANT JUST PUT THE FIBONACCI SPIRAL ON TOP OF WHATEVER THE FUCK YOU WANT","United Witkoland Empire"]}}]`)
 data = data[0].result
 const table = document.querySelectorAll("table")[0]
 for(i = 0; i<data.indexes.length;i++)
